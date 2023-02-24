d3eb3cee350b99af0db15adcc605ff60cb09c7bc
raeesKhan <raeeskhanbuneri58@gmail.com>
2023-02-24 09:13:39 +0500

    updated js html and css file

---
+++ index.js
@@ -0,0 +1,58 @@
+// let name ="raees";
+// let age=23;
+// let myGreet=function(name,age){
+//     if(age===23){
+//         console.log(name,age)
+//     }
+// }
+// let val=myGreet(name,age);
+// console.log(val);
+
+// let myObj={
+//     name:"raees",
+//     age:23,
+//     game:function(){
+//         return "gta"
+//     }
+// }
+// console.log(myObj.game());
+
+
+// // scopes in js
+
+// var i=0;
+// console.log(i);
+// if(i==0){
+//     let i=23;
+//     console.log(i);
+// }
+
+// let name1=function(name,age){
+//        console.log(`my name is ${name} and my age is ${age}`);
+// }
+// name1("raees",23);
+// let val1=name1;
+// console.log(val1);
+
+
+// let a =window;
+// window.prompt("hellow raees");
+// console.log(a);
+
+
+
+
+
+let str='javaScript';
+let links=document.links;
+console.log(links);
+let href;
+Array.from(links).forEach(function(element){
+  href=element.href;
+  if(href.includes(str)){
+    console.log(links);
+  }
+  else{
+    console.log("javascript links not found");
+  }
+});
